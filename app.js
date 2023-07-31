95dfc18a71c3bf8c451e0bb90e4718c165bf0a3c
minodora-c <minodora.cristescu@campus.tu-berlin.de>
2023-07-31 17:26:47 +0200

    419: Campground Model Basics

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const app = express();
-const path = require("path");
 
+const path = require("path");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/Yelp");
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected :)");
+});
+
+const Campground = require("./models/campground");
+
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/makecampground", async (req, res) => {
+  const camp = new Campground({ title: "Garden" });
+  await camp.save();
+  res.send(camp);
 });
 
 app.listen("3000", () => {

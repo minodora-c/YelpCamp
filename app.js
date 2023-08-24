68fdb730fe7e555db05b61c6eb834cbb86c3e5d2
minodora-c <minodora.cristescu@campus.tu-berlin.de>
2023-08-24 12:08:49 +0200

    451-456: error handling (BE)

--- app.js
+++ app.js
@@ -22,59 +22,108 @@ db.once("open", () => {
 });
 
 const Campground = require("./models/campground");
+const catchAsync = require("./utils/catchAsync");
+const ExpressError = require("./utils/ExpressError");
+
+// remember that the order of the paths is important, as the first one that matches the request will be executed
 
 app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/campgrounds", async (req, res) => {
-  const campgrounds = await Campground.find();
-  res.render("campgrounds/index", { campgrounds });
+app.get(
+  "/campgrounds",
+  catchAsync(async (req, res) => {
+    const campgrounds = await Campground.find();
+    res.render("campgrounds/index", { campgrounds });
+  })
+);
+
+app.post(
+  "/campgrounds",
+  catchAsync(async (req, res) => {
+    // you can throw an error and it will be caught by catchAsync and handled in app.use(...)
+    if (!req.body.campground)
+      throw new ExpressError("Invalid Campground Data", 400);
+    let { campground } = req.body;
+    let c = new Campground({
+      title: campground.title,
+      location: campground.location,
+    });
+    await c.save();
+    res.redirect("/campgrounds");
+  })
+);
+
+app.get(
+  "/campgrounds/new",
+  catchAsync(async (req, res) => {
+    res.render("campgrounds/new");
+  })
+);
+
+// try-catch is one way of handling errors
+// however, is there are many paths, wrappinf the function in catchAsync is übersichtlicher und kürzer
+app.get("/campgrounds/:id", async (req, res, next) => {
+  try {
+    let id = req.params.id;
+    const campground = await Campground.findById(id);
+    res.render("campgrounds/show", { campground });
+  } catch (e) {
+    next(e);
+  }
 });
 
-app.post("/campgrounds", async (req, res) => {
-  let { campground } = req.body;
-  let c = new Campground({
-    title: campground.title,
-    location: campground.location,
-  });
-  await c.save();
-  res.redirect("/campgrounds");
+app.put(
+  "/campgrounds/:id",
+  catchAsync(async (req, res) => {
+    let { campground } = req.body;
+    let id = req.params.id;
+    await Campground.findByIdAndUpdate(id, {
+      title: campground.title,
+      location: campground.location,
+      image: campground.image,
+      price: campground.price,
+      description: campground.description,
+    });
+    res.redirect(`/campgrounds/${id}`);
+  })
+);
+
+app.delete(
+  "/campgrounds/:id",
+  catchAsync(async (req, res) => {
+    let id = req.params.id;
+    await Campground.findByIdAndDelete(id);
+    res.redirect("/campgrounds");
+  })
+);
+
+app.get(
+  "/campgrounds/:id/edit",
+  catchAsync(async (req, res) => {
+    let id = req.params.id;
+    const campground = await Campground.findById(id);
+    res.render("campgrounds/edit", { campground });
+  })
+);
+
+// if a random URL that doesn't exist is requested
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
 });
 
-app.get("/campgrounds/new", async (req, res) => {
-  res.render("campgrounds/new");
-});
-
-app.get("/campgrounds/:id", async (req, res) => {
-  let id = req.params.id;
-  const campground = await Campground.findById(id);
-  res.render("campgrounds/show", { campground });
-});
-
-app.put("/campgrounds/:id", async (req, res) => {
-  let { campground } = req.body;
-  let id = req.params.id;
-  await Campground.findByIdAndUpdate(id, {
-    title: campground.title,
-    location: campground.location,
-    image: campground.image,
-    price: campground.price,
-    description: campground.description,
-  });
-  res.redirect(`/campgrounds/${id}`);
-});
-
-app.delete("/campgrounds/:id", async (req, res) => {
-  let id = req.params.id;
-  await Campground.findByIdAndDelete(id);
-  res.redirect("/campgrounds");
-});
-
-app.get("/campgrounds/:id/edit", async (req, res) => {
-  let id = req.params.id;
-  const campground = await Campground.findById(id);
-  res.render("campgrounds/edit", { campground });
+// handling asynchronous errors
+// whether try-catch is used or catchAsync(), both land here if an error is thrown
+app.use((err, req, res, next) => {
+  // const { statusCode = 500, message = "something went wrong :(" } = err;
+  if (!err.message) err.message = "something went wrong :(";
+  res.render("error", { err });
+  // res.status(statusCode).send(message);
+  // res.send("something went wrong :("); // shown to user
+  console.log("called on every call"); // shown here in the terminal (not Chrome)
+  console.log(err);
+  next(); // needed to call the next window (here, res.send(...))
 });
 
 app.listen("3000", () => {
